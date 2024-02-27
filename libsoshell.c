498a3c7a738de11832453f8537d50777952195bb
dah4k <84741290+dah4k@users.noreply.github.com>
2024-02-26 21:51:06 -0500

    Update libsoshell.c
    - Change executable absolute path with relative path in CWD
    - Check return code and add perror()s
    - Add clearer comments

--- libsoshell.c
+++ libsoshell.c
@@ -1,6 +1,8 @@
 // Copyright 2024 dah4k
 // SPDX-License-Identifier: GPL-2.0-only
 
+#include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
@@ -9,6 +11,12 @@ extern int unsetenv(const char *);
 #endif
 
 void __attribute__((constructor)) my_ctor(void) {
-    unsetenv("LD_PRELOAD");         // Critical to avoid small Fork Bomb
-    execl("/bin/busybox-static", "sh", NULL);
+    // Critical to break LD_PRELOAD infinite loop
+    if (unsetenv("LD_PRELOAD") == -1) {
+        perror("unsetenv");
+    }
+    // Execute busybox-static in current working directory
+    if (execl("busybox-static", "sh", NULL) == -1) {
+        perror("execl");
+    }
 }
